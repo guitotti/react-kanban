9fb55b1152c4d1af5244b3ee2278329583049f5c
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-02-09 20:30:39 -0300

    feat: create CreateTaskForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Box, Heading } from "@radix-ui/themes"
+import { Box, Flex, Heading } from "@radix-ui/themes";
+import { CreateTaskForm } from "./components/CreateTaskForm";
 
 function App() {
-
   return (
-    <Box>
-      <Heading>React Kanban</Heading>
+    <Box maxWidth="80rem" mx="auto">
+      <Box height="4rem">
+        <Flex align="center" gap="4" height="100%">
+          <Heading size="8" weight="light">
+            React Kanban
+          </Heading>
+          <CreateTaskForm />
+        </Flex>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
