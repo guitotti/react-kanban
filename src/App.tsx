b5855e0fa4ef301263a167cdba074877a9d05c7d
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-02-16 21:05:17 -0300

    feat: use tasks context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import { Box, Flex, Heading } from "@radix-ui/themes";
 import { CreateTaskForm } from "./components/CreateTaskForm";
 import { TaskBoard } from "./components/TaskBoard";
+import { TasksContextProvider } from "./contexts/TasksContext";
 
 function App() {
   return (
-    <Box maxWidth="80rem" mx="auto">
-      <Box height="4rem">
-        <Flex align="center" gap="4" height="100%">
-          <Heading size="8" weight="light">
-            React Kanban
-          </Heading>
-          <CreateTaskForm />
-        </Flex>
-      </Box>
+    <TasksContextProvider>
+      <Box maxWidth="80rem" mx="auto">
+        <Box height="4rem">
+          <Flex align="center" gap="4" height="100%">
+            <Heading size="8" weight="light">
+              React Kanban
+            </Heading>
+            <CreateTaskForm />
+          </Flex>
+        </Box>
 
-      <Box>
-        <Heading as="h2" mb="4">
-          Quadro de Tarefas
-        </Heading>
-        <TaskBoard />
+        <Box>
+          <Heading as="h2" mb="4">
+            Quadro de Tarefas
+          </Heading>
+          <TaskBoard />
+        </Box>
       </Box>
-    </Box>
+    </TasksContextProvider>
   );
 }
 
